a003f9111210b34c3bdda00fbb7678986e1d9b8b
NIKHIL_RAI <rainikhil760@gmail.com>
2025-02-05 20:01:39 +0530

    Add files via upload

---
+++ day-1/script.js
@@ -0,0 +1,30 @@
+// let js = 'amazing'
+// if (js === 'amazing') {
+//     alert(`javascript is fun`);
+// }
+
+// let firstName = "jonas";//camelcase notation
+// console.log(firstName);
+
+
+
+const now = 2036;
+
+const ageJonas =now - 2003;
+const ageJhon =now - 2002;
+console.log(ageJonas /10 , 2**3);//3 means 3 to power 2
+console.log(ageJhon);
+
+const firstName = "Nikhil";
+const lastName =  "Rai";
+console.log(firstName +' '+lastName);
+//Assginment operator
+let x = 10+5;
+x += 15;
+x++;
+console.log(x);
+
+//comparision opeartor
+
+console.log(ageJonas > ageJhon);
+const isFullAge = ageJhon >=18;
---
+++ day-1/string.js
@@ -0,0 +1,14 @@
+const firstName = "jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}`;
+console.log(jonasNew);
+
+console.log(`string with \n\
+  multiple \n\  `);
+
+
