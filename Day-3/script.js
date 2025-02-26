189ad3a1fd055dc19d6a328d66a0f7866733ca12
NIKHIL_RAI <rainikhil760@gmail.com>
2025-02-26 20:14:05 +0530

    Add files via upload

---
+++ Day-3/script.js
@@ -0,0 +1,13 @@
+ 'use strict';
+
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) {
+    console.log('I can drive');
+    
+}
+// const interface ='Auto'; strict mode reserve this key word
+// cosnt private = 534;
